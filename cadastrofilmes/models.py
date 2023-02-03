37efc779dd69d26d87a8986890de0c1357867c76
Luanlrc <luanlrc@outlook.com>
2023-02-03 15:00:32 -0300

    Criação de models  e migrate

---
+++ cadastrofilmes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Autor(models.Model):
+    nome=models.TextField(max_length=250)
+    idade=models.IntegerField()
+
+class Genero(models.IntegerChoices):
+    terror = 0
+    animacao = 1
+    suspense = 2
+    acao = 3
+ 
+
+class Filme(models.Model):
+    nome= models.TextField()
+    lancamento=models.DateTimeField()
+    descrição=models.TextField()
+    autor=models.ForeignKey(Autor,on_delete=models.DO_NOTHING)
+    genero=models.IntegerField(choices= Genero.choices, null=True)
+
+# Create your models here.
